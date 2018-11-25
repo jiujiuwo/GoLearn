09c067f6a5ad920aff9b35ab346309fc61f50185
jiujiuwo <1203574299@qq.com>
2018-11-25 11:47:18 +0800

    go语言，正则库，结果错误，两种方法都试过了

--- leetcode_10.go
+++ leetcode_10.go
@@ -7,9 +7,11 @@ import (
 
 /*
 	同样是内建的正则库，go语言的结果与java不同，不知道为什么。
+	目前该方法是错误的，下一步寻找问题
  */
 func isMatch(s string, p string) bool {
 	isMatch,_ := regexp.MatchString(p,s)
+	//isMatch,_ := regexp.Match(p,[]byte(s))	两个方法都是错的
 	return isMatch
 }
 
