89b7fea9a37dc51902e1c3ffc04b63601f8fe5c0
jiujiuwo <1203574299@qq.com>
2018-12-04 20:32:46 +0800

    通过提前排序，将时间缩短了一些，但是还是超时

--- leetcode_15.go
+++ leetcode_15.go
@@ -6,35 +6,34 @@ import (
 )
 
 func threeSum(nums []int) [][]int {
-
-	result := make([][]int,len(nums)*len(nums))
 	length := len(nums)
+
+	if length<=0{
+		return nil
+	}
+	result := make([][]int,len(nums)*len(nums))
 	index :=0
 
-	if(length<=0){
-		return result
-	}
+	sort.Ints(nums)
 
 	for i:=0;i<length;i++{
-		for j:=0;j<length;j++{
-			for k:=0;k<length;k++{
-				if nums[i] + nums[j] + nums[k] == 0 && i !=j && j!=k && i!=k{
+		for j:=i+1;j<length;j++{
+			for k:=j+1;k<length;k++{
+				if nums[i] + nums[j] + nums[k] == 0{
 					result[index] = make([]int,3)
 					result[index][0] = nums[i]
 					result[index][1] = nums[j]
 					result[index][2] = nums[k]
 					if index>0{
-						sort.Ints(result[index])
+						//sort.Ints(result[index])
 						dup := false
 						//为什么是从0到 index+1呢
 						for m:=0;m<index;m++{
-							sort.Ints(result[m])
-							fmt.Println(m,index)
+							//sort.Ints(result[m])
+							//fmt.Println(m,index)
 							if result[m][0]==result[index][0]&&result[m][1]==result[index][1]&&result[m][2]==result[index][2]{
 								dup = true
 								break
-							}else{
-								continue
 							}
 						}
 						if !dup{
